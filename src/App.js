9550b3a9989eace1742c2a740add6c691383001e
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2023-01-24 18:13:38 +0200

    Added the css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <article>
+        <h1>Image to Text</h1>
+        <form>
+          <input type='file' accept='png/jpg/jpeg/gif' />
+          <input type='submit' value='Convert' />
+        </form>
+        <h3>Result:</h3>
+        <p></p>
+      </article>
+    </section>
   );
 }
 
